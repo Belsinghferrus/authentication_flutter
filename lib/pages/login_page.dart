f80509826c573ef9cd6c210e6887cda65bff419f
Belsinghferrus <ferrusbelsingh@gmail.com>
2024-09-24 13:01:57 +0530

    forgot pass

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,32 +1,83 @@
+import 'package:authentication_flutter/components/my_textfield.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  //text editing controller
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
-      body: Column(
-        children: [
-          //logo
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
 
-          //welcome
+              //logo
+              const Icon(
+                Icons.lock,
+                size: 100,
+              ),
+              const SizedBox(height: 50),
 
-          //username textfield
+              //welcome
+              Text(
+                "Welcome back you've been missed!",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey.shade700,
+                ),
+              ),
+              const SizedBox(height: 25),
 
-          //password text
+              //username textfield
+              MyTextfield(
+                hintText: "username",
+                obscureText: false,
+                controller: usernameController,
+              ),
 
-          //forgot password
+              const SizedBox(height: 10),
+              //password text
+              MyTextfield(
+                hintText: "password",
+                obscureText: true,
+                controller: passwordController,
+              ),
+              const SizedBox(height: 10),
 
-          //sign in button
+              //forgot password
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Forgot password?",
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 25),
 
-          //or continue with
+              //sign in button
 
-          //google or apple
+              //or continue with
 
-          // not a member? register now
-        ],
+              //google or apple
+
+              // not a member? register now
+            ],
+          ),
+        ),
       ),
     );
   }
