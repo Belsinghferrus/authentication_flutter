66b8a3dc36b31c1f0f1d08a7a75ad57991791d6e
Belsinghferrus <ferrusbelsingh@gmail.com>
2024-09-24 11:34:06 +0530

    Initial  commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      body: Column(
+        children: [
+          //logo
+
+          //welcome
+
+          //username textfield
+
+          //password text
+
+          //forgot password
+
+          //sign in button
+
+          //or continue with
+
+          //google or apple
+
+          // not a member? register now
+        ],
+      ),
+    );
+  }
+}
